22788f767ab80f664e0baffce7b9324823d59df6
Jillian <jngierke@gmail.com>
2014-02-18 11:59:35 -0500

    login page, started logout

--- login.php
+++ login.php
@@ -1,3 +1,76 @@
 <?php
 include("base.php");
+print_r($_POST);
+if(!empty($_POST['username']) && !empty($_POST['password']))
+{
+	$username = mysql_real_escape_string($_POST['username']);
+    $password = md5($_POST['password']);
+    $checklogin = mysql_query("SELECT * FROM users WHERE Username = '".$username."' AND Password = '".$password."'") or die('Query failed: ' . mysql_error());
 
+    if(mysql_num_rows($checklogin) == 1)
+    {
+    	$row = mysql_fetch_assoc($checklogin);
+        
+        $_SESSION['Username'] = $username;
+        $_SESSION['LoggedIn'] = true;
+        echo "<h1>Welcome!</h1>";
+        echo "<meta http-equiv='refresh' content='units.php' />";
+        exit();
+    }
+    else
+    {
+    	echo "<h1>Error</h1>";
+        echo "<p>Oops, wrong username or password. Please <a href=\"index.php\">click here to try again</a>.</p>";
+    }
+}
+	?>
+	
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Around The City</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <form class="form-horizontal" role="form" action="login.php" method="post">
+  <div class="form-group">
+    <label for="inputUsername" class="col-sm-2 control-label">Username</label>
+    <div class="col-sm-10">
+      <input type="username" class="form-control" name="username" id="inputUsername" placeholder="Username">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
+  
+  
+  
+  
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+include("base.php");
+
+	session_start();
+	session_destroy();
+	$_SESSION = array();
+?>
+<meta content="0;index.php" http-equiv="refresh">
