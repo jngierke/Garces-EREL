8a21e3206d37e7c7bcc34c355c2a88ca155e3fef
Jillian <jngierke@gmail.com>
2014-02-18 12:56:47 -0500

    fixed title for login page, corrected title for unit 5

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ if(!empty($_POST['username']) && !empty($_POST['password']))
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Around The City</title>
+    <title>Garces EREL Vocabulary</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
